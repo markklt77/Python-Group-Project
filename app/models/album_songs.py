9e26039b330d95ec30fa7bc5e200fad3538f4d83
abraham <agarciasantos14@gmail.com>
2024-11-04 20:56:15 -0800

    seeder data

--- app/models/album_songs.py
+++ app/models/album_songs.py
@@ -7,6 +7,3 @@ class AlbumSong(db.Model):
     song_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("songs.id")), nullable=False)
     album_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("albums.id")), nullable=False)
     added_at = db.Column(db.DateTime, server_default=db.func.now())
-
-    song = db.relationship("Song", back_populates="albums")
-    album = db.relationship("Album", back_populates="songs")
