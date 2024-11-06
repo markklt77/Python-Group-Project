4a6cb51d847cd44fd69695e399417127909a6b1b
david <dav94sal@gmail.com>
2024-11-05 20:09:53 -0600

    Add routes for GET and POST and an instance method for the Song model

--- app/api/song_routes.py
+++ app/api/song_routes.py
@@ -1,15 +1,24 @@
-from flask import Blueprint
-from ..models.song import Song
+from flask import Blueprint, request
+from ..models.song import db, Song
 
 bp = Blueprint('songs', __name__, url_prefix='/songs')
 
 @bp.route('/')
 def songs():
     songs = Song.query.all()
-    return songs
+    return [song.to_dict() for song in songs]
 
 
-@bp.route('/:songId')
+@bp.route('/<songId>')
 def songId(songId):
-    song = Song.get(songId)
-    return song
+    song = Song.query.get(songId)
+    return song.to_dict()
+
+
+@bp.route('/', methods=["POST"])
+def addSong():
+    data = request.json
+    song = Song(**data)
+    db.session.add(song)
+    db.session.commit()
+    return song.to_dict()
