c9ff05e8b5bcab601488e3cab36192842a4c564c
david <dav94sal@gmail.com>
2024-11-15 17:46:43 -0600

    Play functionality complete

--- react-vite/src/components/SongTile/SongTile.jsx
+++ react-vite/src/components/SongTile/SongTile.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { likeSong, unlikeSong } from "../../redux/likes";
 import { CiCirclePlus } from "react-icons/ci";
 import { FaHeart } from "react-icons/fa";
-import "./song-tile.css"
+import { getOneSong } from "../../redux/songs";
+import { IoPlaySharp } from "react-icons/io5";
+import "./song-tile.css";
 
 function SongTile({ song, number }) {
     const [liked, setLiked] = useState(false)
     const [likesCount, setLikesCount] = useState(0)
+    const [hovered, setHovered] = useState(false)
     const dispatch = useDispatch()
 
+    // format month
     const date = new Date(song.created_at)
     const months = [
         "Jan", "Feb", "Mar", "Apr", "May", "Jun",
@@ -17,8 +21,23 @@ function SongTile({ song, number }) {
     ]
     const month = months[date.getMonth()]
 
-    console.log(song.likes.length)
-    
+
+    useEffect(() => {
+        // reference to SongTile
+        const tiles = document.getElementsByClassName("song-tile")
+        const tile = tiles[number-1]
+
+        tile.addEventListener("mouseenter", () => {
+            setHovered(true)
+        })
+
+        tile.addEventListener("mouseleave", () => {
+            setHovered(false)
+        })
+    })
+
+    // console.log(song.likes.length)
+
     const handleLike = async (e) => {
         e.preventDefault();
 
@@ -39,9 +58,23 @@ function SongTile({ song, number }) {
         .then(setLikesCount(likesCount => likesCount - 1))
     }
 
+    const handleClick = () => {
+        dispatch(getOneSong(song.id))
+    }
+
     return (
-        <div className="song-tile">
-            <p id="num">{number}</p>
+        <div className="song-tile" key={`song${song.id}`}>
+            { hovered?
+                <div id="play-button-container" >
+                    <button
+                        onClick={handleClick}
+                        className="play-button"
+                    >
+                        <IoPlaySharp />
+                    </button>
+                </div> :
+                <p id="num">{number}</p>
+            }
             <p>{song.title}</p>
             <p>{song.album.title}</p>
             <p>
@@ -52,7 +85,7 @@ function SongTile({ song, number }) {
                 <FaHeart className="like-button" onClick={ !liked ? handleLike : handleUnlike} style={ liked ? {color: "rgb(54, 58, 121)"} : ''}/>
                 <span className="likes-count">{likesCount}</span>
             </div>
-            
+
         </div>
     )
 }
