eaa3d2288b0c33e4666b6e33313b2d0f2ec0bc8b
markklt77 <markklt77@gmail.com>
2024-11-18 11:59:34 -0800

    positioning changes for trash button

--- react-vite/src/components/Playlists/PlaylistTile.jsx
+++ react-vite/src/components/Playlists/PlaylistTile.jsx
@@ -9,12 +9,15 @@ function PlaylistTile({user, name, playlistId, onClick, handleDelete}) {
         onClick={onClick}>
             <div className="top-part">
                 <h4>{name}</h4>
-                <button onClick={(e) => {
-                    e.stopPropagation();
-                    handleDelete(playlistId);
-                }} className="delete-button-playlist">
-                    <IoTrashSharp />
-                </button>
+                <div>
+                    <button onClick={(e) => {
+                        e.stopPropagation();
+                        handleDelete(playlistId);
+                    }} className="delete-button-playlist">
+                        <IoTrashSharp />
+                    </button>
+                </div>
+
             </div>
             <p>Playlist . {user}</p>
         </div>
